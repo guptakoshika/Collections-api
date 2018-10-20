4724ab80df8c0a5ad711242e80ca85ed8fba86e8
Koshika Gupta <koshikaje@gmail.com>
2018-10-20 11:04:16 +0530

    Update LinkedList.java

--- Uca.Collections/src/Uca/Collections/LinkedList.java
+++ Uca.Collections/src/Uca/Collections/LinkedList.java
@@ -1,63 +1,65 @@
-
 package Uca.Collections;
 
-public class LinkedList<T> implements List<Object>{
-    private class node<T>{
+public class LinkedList<T> implements List<Object> {
+
+    private class node<T> {
+
         T data;
-        T next ;
-        public node(T d){
+        node next;
+
+        public node(T d) {
             data = d;
             next = null;
         }
     }
     private node head;
-    
+
     @Override
     public void removeSomeIndex() {
-        
+
     }
 
     @Override
     public Object getFromIndex(int index) {
         node temp = head;
         int c = 0;
-        while(temp.next!= null && c!=index)
-        {
+        while (temp.next != null && c != index) {
             temp = temp.next;
             c++;
         }
         return temp.data;
     }
 
-    @Override
-    public void add(Comparable<Object> t) {
-        
-    }
-
-    @Override
-    public void remove(Comparable<Object> t) {
-        
-    }
 
     @Override
     public boolean isEmpty() {
-       if(head == null)
-           return true;
-       return false;
+        if (head == null) {
+            return true;
+        }
+        return false;
     }
 
     @Override
-    public boolean clear() {
-     return null;   
+    public void clear() {
+        head = null;
     }
 
     @Override
-    public int size(Comparable<Object> t) {
+    public void add(Object t) {
         
     }
 
+    @Override
+    public void remove(Object t) {
+        
+    }
+
+    @Override
+    public int size(Object t) {
+        
+    }
     @Override
     public Iterator iterator() {
-        
+
     }
 }
