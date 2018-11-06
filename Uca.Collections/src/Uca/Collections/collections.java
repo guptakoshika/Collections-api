2fa49d5f19e918a26fdbfb5d16cc239858ad014b
Koshika Gupta <koshikaje@gmail.com>
2018-11-06 20:47:57 +0530

    Update collections.java

--- Uca.Collections/src/Uca/Collections/collections.java
+++ Uca.Collections/src/Uca/Collections/collections.java
@@ -1,10 +1,9 @@
 package Uca.Collections;
 
-public interface collections<T extends Comparable<T>> extends Iterable<T> {
+public interface Collections<T> extends Iterable<T> {
     public void add(T t);
     public void remove(T t);
     public boolean isEmpty();
     public void clear();
-    public boolean isFull();
-    public int size(T t);
+    public int size();
 }
