629603783b5083ab0d93ec69d0cc86f067199c6a
Koshika Gupta <koshikaje@gmail.com>
2018-11-06 20:46:55 +0530

    Update LinkedList.java

--- Uca.Collections/src/Uca/Collections/LinkedList.java
+++ Uca.Collections/src/Uca/Collections/LinkedList.java
@@ -1,8 +1,8 @@
 package Uca.Collections;
 
-public class LinkedList<T> implements List<Object> {
+public class LinkedList<T> implements List<T> {
 
-    private class node<T> {
+    private class node {
 
         T data;
         node next;
@@ -13,23 +13,67 @@ public class LinkedList<T> implements List<Object> {
         }
     }
     private node head;
+    int index;
 
-    @Override
-    public void removeSomeIndex() {
-
+    public LinkedList() {
+        index = 0;
     }
 
     @Override
-    public Object getFromIndex(int index) {
+    public void add(T t) {
         node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        node n = new node(t);
+        temp.next = n;
+        head = temp;
+        index++;
+    }
+
+    @Override
+    public void remove(T t) {
+        node temp = head;
+        while (temp != null && temp != t) {
+            temp = temp.next;
+        }
+        if (temp == null) {
+            return;
+        }
+    }
+
+    @Override
+    public int size() {
+        return index;
+    }
+
+    @Override
+    public void removeSomeIndex(int index) {
+        node temp = head;
+        node prev = null;
         int c = 0;
+        while (temp.next != null && c != index) {
+            prev = temp;
+            temp = temp.next;
+            c++;
+        }
+        if (temp.next != null) {
+            prev.next = temp.next;
+        }
+        head = prev;
+    }
+
+    @Override
+    public T getFromIndex(int index) {
+
+        node temp = head;
+        Integer c = 0;
         while (temp.next != null && c != index) {
             temp = temp.next;
             c++;
         }
         return temp.data;
     }
-
 
     @Override
     public boolean isEmpty() {
@@ -42,24 +86,25 @@ public class LinkedList<T> implements List<Object> {
     @Override
     public void clear() {
         head = null;
+        index = 0;
     }
 
     @Override
-    public void add(Object t) {
-        
-    }
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private node t = head;
 
-    @Override
-    public void remove(Object t) {
-        
-    }
+            @Override
+            public boolean hasNext() {
+                return t != null;
+            }
 
-    @Override
-    public int size(Object t) {
-        
-    }
-    @Override
-    public Iterator iterator() {
-
+            @Override
+            public T Next() {
+                T e = t.data;
+                t = t.next;
+                return e;
+            }
+        };
     }
 }
